4fdefbe0b6c0b232acadf9b485f6fb9e1b28257e
Brandon Vergara <leovergara.dark@gmail.com>
2020-09-26 08:25:33 -0500

    coding master  creating services to subtract, add, divide and multiplicación

--- src/main/java/com/operation/calculator/controller/CalculatorController.java
+++ src/main/java/com/operation/calculator/controller/CalculatorController.java
@@ -1,20 +1,36 @@
 package com.operation.calculator.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.operation.calculator.model.Calculator;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	@GetMapping("/suma")
+	public Calculator suma(@RequestParam(value = "firstParam", defaultValue = "0") float firstParam, @RequestParam(value = "secondParam", defaultValue = "0") float secondParam) {
+		float result = firstParam + secondParam;
+		return new Calculator(result);
+	}
 
-	@GetMapping("/calculator")
-	public Calculator calculator(@RequestParam(value = "firstParam", defaultValue = "0") String firstParam, @RequestParam(value = "secondParam", defaultValue = "0") String secondParam) {
-		return new Calculator(10);
+	@GetMapping("/resta")
+	public Calculator resta(@RequestParam(value = "firstParam", defaultValue = "0") float firstParam, @RequestParam(value = "secondParam", defaultValue = "0") float secondParam) {
+		float result = firstParam - secondParam;
+		return new Calculator(result);
+	}
+
+	@GetMapping("/multiplicacion")
+	public Calculator multiplicacion(@RequestParam(value = "firstParam", defaultValue = "0") float firstParam, @RequestParam(value = "secondParam", defaultValue = "0") float secondParam) {
+		float result = firstParam * secondParam;
+		return new Calculator(result);
+	}
+
+	@GetMapping("/division")
+	public Calculator division(@RequestParam(value = "firstParam", defaultValue = "0") float firstParam, @RequestParam(value = "secondParam", defaultValue = "0") float secondParam) {
+		float result = firstParam / secondParam;
+		return new Calculator(result);
 	}
 }
